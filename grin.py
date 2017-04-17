a528ba977342c7eb85c45857187e88653e956ea3
Raffaele Salmaso <raffaele@salmaso.org>
2017-04-17 23:31:33 +0200

    added VERSION/get_version and update __version__ generation

--- grin.py
+++ grin.py
@@ -15,8 +15,34 @@ import stat
 import sys
 from io import UnsupportedOperation
 
+
+VERSION = (1, 2, 1, 'final', 0)
+
+
+def get_version(version=None):
+    "Returns a PEP 386-compliant version number from VERSION."
+    assert len(version) == 5
+    assert version[3] in ('alpha', 'beta', 'rc', 'final')
+
+    # Now build the two parts of the version number:
+    # main = X.Y[.Z]
+    # sub = .devN - for pre-alpha releases
+    #     | {a|b|c}N - for alpha, beta and rc releases
+
+    parts = 2 if version[2] == 0 else 3
+    main = '.'.join(str(x) for x in version[:parts])
+
+    sub = ''
+
+    if version[3] != 'final':
+        mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
+        sub = mapping[version[3]] + str(version[4])
+
+    return str(main + sub)
+
+
 # Constants
-__version__ = '1.2.1'
+__version__ = get_version(VERSION)
 
 # Maintain the numerical order of these constants. We use them for sorting.
 PRE = -1
