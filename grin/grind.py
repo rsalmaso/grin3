fafb67d3ca2170c35ada48e337591a44f812687f
Raffaele Salmaso <raffaele@salmaso.org>
2020-08-02 10:28:16 +0200

    Update copyright year

--- grin/grind.py
+++ grin/grind.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019, Raffaele Salmaso <raffaele@salmaso.org>
+# Copyright (C) 2017-2020, Raffaele Salmaso <raffaele@salmaso.org>
 # Copyright (C) 2007, Enthought, Inc.
 # All rights reserved.
 #
