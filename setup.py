5ad41969c114fcdf909f4b9cd0cc74aa688d7c57
Raffaele Salmaso <raffaele@salmaso.org>
2021-01-07 00:49:12 +0100

    Use declarative config in setup.cfg

--- setup.py
+++ setup.py
@@ -1,37 +1,4 @@
-import io
-
-import grin
-from setuptools import find_packages, setup
-
-# Read the long description from the README.txt
-with io.open("README.rst", "rt") as f:
-    long_description = f.read()
+from setuptools import setup
 
 
-setup(
-    name="grin3",
-    version=grin.__version__,
-    author=grin.__author__,
-    author_email=grin.__author_email__,
-    maintainer=grin.__maintainer__,
-    maintainer_email=grin.__maintainer_email__,
-    description="A grep program configured the way I like it. (python3 port)",
-    long_description=long_description,
-    license="BSD",
-    url="https://bitbucket.org/rsalmaso/grin3",
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Topic :: Utilities",
-    ],
-    packages=find_packages(),
-    entry_points=dict(console_scripts=["grin = grin.grin:main", "grind = grin.grind:main"]),
-)
+setup()
